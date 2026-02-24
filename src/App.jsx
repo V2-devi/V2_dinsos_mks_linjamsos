9ad6f43cccc05409f1dc719a3944fb9f41f3bf61
firlianyfirdaus <firlianyfirdaus853@gmail.com>
2026-02-24 16:36:06 +0800

    folder struktur

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Website Dinas Sosial - Linjamsos</h1>
+      <p>Sistem Pendataan dan Pengelolaan Bansos dan PPKS</p>
+    </div>
+  )
+}
+
+export default App
